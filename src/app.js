574cedf3618ad6b6c61dcfcfaacf50d4f9c5eb68
Lucca Lopes Rodrigues <m1515080@pmg.goiania.go.gov.br>
2023-03-14 09:01:48 -0300

    second commit

--- src/app.js
+++ src/app.js
@@ -2,9 +2,12 @@ const express = require('express')
 const app = express()
 
 
+app.use('/css', express.static(__dirname + '/css'))
+app.use('/js', express.static(__dirname + '/js'))
+app.use('/imagens', express.static(__dirname + '/imagens'))
 
 app.listen(3000, function () {
-    console.log('Servidor iniciado na porta 3000');
+    console.log('http://localhost:3000/');
 });
 
 app.get('/', function (req, res) {
